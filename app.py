93a68129674019442455cfca33811e3290f4d7ab
Oleh Dubno <olehdubno@gmail.com>
2018-02-16 00:21:43 -0500

    more changes

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+from nocache import nocache
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
+@nocache
 def index():
 	return render_template('index.html')
 
 @app.route('/quantitative')
+@nocache
 def quantitative():
     return render_template('quantitative.html')
 
 @app.route('/qualitative')
+@nocache
 def qualitative():
     return render_template('qualitative.html')
 
 @app.route('/about')
+@nocache
 def about():
     return render_template('about.html')
 
