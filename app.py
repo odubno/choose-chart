898f8cbd8e72cb4b9eddf535e7cfe8dcb2d33c4a
Oleh Dubno <olehdubno@gmail.com>
2018-02-15 22:39:08 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/quantitative')
+def quantitative():
+    return render_template('quantitative.html')
+
+@app.route('/qualitative')
+def qualitative():
+    return render_template('qualitative.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
